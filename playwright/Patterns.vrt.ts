3c9ab3112201b8da6a05e882a6f912dad62bbb37
Anton Schmidt <anton.schmidt@onecode.de>
2025-08-28 15:18:12 +0200

    feat: run build:copyPatterns | as

--- playwright/Patterns.vrt.ts
+++ playwright/Patterns.vrt.ts
@@ -1,7 +1,7 @@
 import { runVisualRegressionTest } from './helpers/helper';
 
+runVisualRegressionTest('patterns/Forms/Layout/FormLayout', {fileNamePrefix: 'FormLayout-'});
 runVisualRegressionTest('patterns/Header/BrandAddOn/HeaderPatternBrandAddOn', {fileNamePrefix: 'HeaderPatternBrandAddOn-'});
 runVisualRegressionTest('patterns/Header/Complete/HeaderPatternComplete', {fileNamePrefix: 'HeaderPatternComplete-'});
 runVisualRegressionTest('patterns/Header/MegaMenu/HeaderPatternMegaMenu', {fileNamePrefix: 'HeaderPatternMegaMenu-'});
 runVisualRegressionTest('patterns/Header/PrimaryArea/HeaderPatternPrimaryArea', {fileNamePrefix: 'HeaderPatternPrimaryArea-'});
-runVisualRegressionTest('patterns/Forms/Layout/FormLayout', {fileNamePrefix: 'FormLayout-'});
