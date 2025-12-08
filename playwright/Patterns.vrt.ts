f92a2c78009b4915ce5a976649cfa80cdfe04ffc
Anton Schmidt <anton.schmidt@onecode.de>
2025-12-08 11:34:58 +0100

    feat: apply sorted vrt paths | as

--- playwright/Patterns.vrt.ts
+++ playwright/Patterns.vrt.ts
@@ -1,9 +1,9 @@
 import { runVisualRegressionTest } from './helpers/helper';
 
-runVisualRegressionTest('patterns/Forms/Layout/FormLayout', {fileNamePrefix: 'FormLayout-'});
-runVisualRegressionTest('patterns/Header/Complete/HeaderPatternComplete', {fileNamePrefix: 'HeaderPatternComplete-'});
-runVisualRegressionTest('patterns/Header/BrandAddOn/HeaderPatternBrandAddOn', {fileNamePrefix: 'HeaderPatternBrandAddOn-'});
 runVisualRegressionTest('patterns/Forms/FormLibraries/FormLibrariesFormik', {fileNamePrefix: 'FormLibrariesFormik-'});
 runVisualRegressionTest('patterns/Forms/FormLibraries/FormLibrariesReactHookForm', {fileNamePrefix: 'FormLibrariesReactHookForm-'});
-runVisualRegressionTest('patterns/Header/PrimaryArea/HeaderPatternPrimaryArea', {fileNamePrefix: 'HeaderPatternPrimaryArea-'});
+runVisualRegressionTest('patterns/Forms/Layout/FormLayout', {fileNamePrefix: 'FormLayout-'});
+runVisualRegressionTest('patterns/Header/BrandAddOn/HeaderPatternBrandAddOn', {fileNamePrefix: 'HeaderPatternBrandAddOn-'});
+runVisualRegressionTest('patterns/Header/Complete/HeaderPatternComplete', {fileNamePrefix: 'HeaderPatternComplete-'});
 runVisualRegressionTest('patterns/Header/MegaMenu/HeaderPatternMegaMenu', {fileNamePrefix: 'HeaderPatternMegaMenu-'});
+runVisualRegressionTest('patterns/Header/PrimaryArea/HeaderPatternPrimaryArea', {fileNamePrefix: 'HeaderPatternPrimaryArea-'});
