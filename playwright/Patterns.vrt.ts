430d49cd6e2d803d68a5e015021faa928ee36cc1
Stefan Feser <stefan@stefanfeser.com>
2025-12-22 13:24:41 +0100

    test: update LinkCard vrt snaps | sf | DS-1127

--- playwright/Patterns.vrt.ts
+++ playwright/Patterns.vrt.ts
@@ -1,11 +1,32 @@
 import { runVisualRegressionTest } from './helpers/helper';
 
-runVisualRegressionTest('patterns/Cards/LinkCardGrid/LinkCardGridPattern', {fileNamePrefix: 'LinkCardGridPattern-'});
-runVisualRegressionTest('patterns/Cards/LinkCardResponsive/LinkCardResponsivePattern', {fileNamePrefix: 'LinkCardResponsivePattern-'});
-runVisualRegressionTest('patterns/Forms/FormLibraries/FormLibrariesFormik', {fileNamePrefix: 'FormLibrariesFormik-'});
-runVisualRegressionTest('patterns/Forms/FormLibraries/FormLibrariesReactHookForm', {fileNamePrefix: 'FormLibrariesReactHookForm-'});
-runVisualRegressionTest('patterns/Forms/Layout/FormLayout', {fileNamePrefix: 'FormLayout-'});
-runVisualRegressionTest('patterns/Header/BrandAddOn/HeaderPatternBrandAddOn', {fileNamePrefix: 'HeaderPatternBrandAddOn-'});
-runVisualRegressionTest('patterns/Header/Complete/HeaderPatternComplete', {fileNamePrefix: 'HeaderPatternComplete-'});
-runVisualRegressionTest('patterns/Header/MegaMenu/HeaderPatternMegaMenu', {fileNamePrefix: 'HeaderPatternMegaMenu-'});
-runVisualRegressionTest('patterns/Header/PrimaryArea/HeaderPatternPrimaryArea', {fileNamePrefix: 'HeaderPatternPrimaryArea-'});
+runVisualRegressionTest('patterns/Cards/LinkCardGrid/LinkCardGrid', {
+  fileNamePrefix: 'LinkCardGrid-',
+});
+runVisualRegressionTest(
+  'patterns/Cards/LinkCardResponsive/LinkCardResponsive',
+  { fileNamePrefix: 'LinkCardResponsive-' }
+);
+runVisualRegressionTest('patterns/Forms/FormLibraries/FormLibrariesFormik', {
+  fileNamePrefix: 'FormLibrariesFormik-',
+});
+runVisualRegressionTest(
+  'patterns/Forms/FormLibraries/FormLibrariesReactHookForm',
+  { fileNamePrefix: 'FormLibrariesReactHookForm-' }
+);
+runVisualRegressionTest('patterns/Forms/Layout/FormLayout', {
+  fileNamePrefix: 'FormLayout-',
+});
+runVisualRegressionTest('patterns/Header/BrandAddOn/HeaderPatternBrandAddOn', {
+  fileNamePrefix: 'HeaderPatternBrandAddOn-',
+});
+runVisualRegressionTest('patterns/Header/Complete/HeaderPatternComplete', {
+  fileNamePrefix: 'HeaderPatternComplete-',
+});
+runVisualRegressionTest('patterns/Header/MegaMenu/HeaderPatternMegaMenu', {
+  fileNamePrefix: 'HeaderPatternMegaMenu-',
+});
+runVisualRegressionTest(
+  'patterns/Header/PrimaryArea/HeaderPatternPrimaryArea',
+  { fileNamePrefix: 'HeaderPatternPrimaryArea-' }
+);
